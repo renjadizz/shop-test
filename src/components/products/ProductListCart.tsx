b76e5d7353bf986c5d5064cdbb9a50f982122acf
Murat <renja.dizz@gmail.com>
2023-02-06 21:40:44 +0600

    add ProductsListCart component

---
+++ src/components/products/ProductListCart.tsx
@@ -0,0 +1,11 @@
+import {useDispatch} from "react-redux";
+import {Box, Button, Typography} from "@mui/material";
+
+export const ProductListCart = () => {
+
+    return (
+        <Box sx={{}}>
+
+        </Box>
+    )
+}
