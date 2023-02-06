4d7e79a9c084e149bd3b98caceed4ca25012933b
Murat <renja.dizz@gmail.com>
2023-02-06 21:41:14 +0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Base from './pages/Base';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {Home} from "./pages/Home";
+import {store} from "./store/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Base/>}>
+                    <Route path="/" element={<Home/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </Provider>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
